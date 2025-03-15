a62df8f7d32bff03fb467e5e407a466b251d2314
Andrii Kityk <kityk.andry@gmail.com>
2025-03-15 23:46:33 +0200

    Add burger-menu

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -13,3 +13,21 @@ else if (strPath[6] === 't')
 {
     lsNav[2].style.color = "#3b82f6";
 }
+
+document.getElementById("burger-menu-show").addEventListener("click", btnBurgerMenuShowClicked);
+
+let isNavShowed = false;
+function btnBurgerMenuShowClicked()
+{
+    if (isNavShowed)
+    {
+        document.querySelector("nav").style.display = "none";
+    }
+    else
+    {
+        document.querySelector("nav").style.display = "block";
+    }
+    isNavShowed = !isNavShowed;
+}
+
+
